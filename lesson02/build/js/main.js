543ef4af532f422076db7bd138835929d939e85c
Sumeet Prajapati <sumeetprajapati15@gmail.com>
2023-04-24 10:59:38 +0530

    lesson2 - Types

---
+++ lesson02/build/js/main.js
@@ -0,0 +1,15 @@
+"use strict";
+let myName = "sumeet";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "Sumeet";
+meaningOfLife = 42;
+isLoading = true;
+album = 1996;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
---
+++ lesson02/src/main.ts
@@ -0,0 +1,17 @@
+let myName: string = "sumeet";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: number | string;
+
+myName = "Sumeet";
+meaningOfLife = 42;
+isLoading = true;
+album = 1996;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+let re: RegExp = /\w+/g;
